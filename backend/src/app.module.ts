ad1828abe727ce7123ce12fdf576eb5d234b6991
gustavobarez <gbarez44@gmail.com>
2025-10-16 19:30:12 -0300

    feat: create folders for entities

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DonorsModule } from './donors/donors.module';
+import { InstitutionsModule } from './institutions/institutions.module';
+import { DonationsModule } from './donations/donations.module';
 
 @Module({
-  imports: [],
+  imports: [DonorsModule, InstitutionsModule, DonationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
