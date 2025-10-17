a0f509129551b63b239ff6f5852ffa17ccd8f440
Gabriel bertolini <gabrielbertolinibarioni@gmail.com>
2025-10-16 22:25:42 -0300

    feat: cadastro de doações

--- backend/src/donations/donations.controller.ts
+++ backend/src/donations/donations.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { DonationsService } from './donations.service';
+import type { InferInsertModel } from 'drizzle-orm';
+import { donations } from 'src/db/schema';
 
 @Controller('donations')
-export class DonationsController {}
+export class DonationsController {
+  constructor(private readonly donationsService: DonationsService) {}
+
+  @Get()
+  async findAll() {
+    return this.donationsService.findAll();
+  }
+
+  @Get('donor/:id')
+  async findByDonor(@Param('id') id: number) {
+    return this.donationsService.findByDonor(Number(id));
+  }
+
+  @Get('institution/:id')
+  async findByInstitution(@Param('id') id: number) {
+    return this.donationsService.findByInstitution(Number(id));
+  }
+
+  @Post()
+  async create(@Body() donationData: InferInsertModel<typeof donations>) {
+    return this.donationsService.create(donationData);
+  }
+
+  @Patch(':id')
+  async updateStatus(@Param('id') id: number, @Body('status') status: string) {
+    return this.donationsService.updateStatus(Number(id), status);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    return this.donationsService.delete(Number(id));
+  }
+}
--- backend/src/donations/donations.service.ts
+++ backend/src/donations/donations.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InferInsertModel, eq } from 'drizzle-orm';
+import { db } from 'src/db/database.module';
+import { donations } from 'src/db/schema';
 
 @Injectable()
-export class DonationsService {}
+export class DonationsService {
+  async findAll() {
+    return db.select().from(donations);
+  }
+
+  async findByInstitution(institutionId: number) {
+    return db.select().from(donations).where(eq(donations.institutionId, institutionId));
+  }
+
+  async findByDonor(donorId: number) {
+    return db.select().from(donations).where(eq(donations.donorId, donorId));
+  }
+
+  async create(donationData: InferInsertModel<typeof donations>) {
+    const result = await db.insert(donations).values(donationData).returning();
+    return result[0];
+  }
+
+  async updateStatus(id: number, status: string) {
+    const result = await db
+      .update(donations)
+      .set({ status })
+      .where(eq(donations.id, id))
+      .returning();
+
+    return result[0];
+  }
+
+  async delete(id: number) {
+    const result = await db.delete(donations).where(eq(donations.id, id)).returning();
+    return result[0];
+  }
+}
