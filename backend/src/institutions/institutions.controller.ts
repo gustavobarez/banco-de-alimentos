8ffd6a88eeb96c3de20a520f3c3b7f7e839ea972
Gabriel bertolini <gabrielbertolinibarioni@gmail.com>
2025-10-16 21:43:22 -0300

    feat: cadastro de instituições

--- backend/src/institutions/institutions.controller.ts
+++ backend/src/institutions/institutions.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { InstitutionsService } from './institutions.service';
 
 @Controller('institutions')
-export class InstitutionsController {}
+export class InstitutionsController {
+    constructor(private readonly institutionsService: InstitutionsService) { }
+    
+        @Get('findAllInstitutions')
+        getAll() {
+            console.log('Doadores achados:', this.institutionsService.findAll())
+            return this.institutionsService.findAll();
+        }
+    
+        @Post('createInstitution')
+        async create(@Body() body: any) {
+            return this.institutionsService.create(body);
+        }
+}
--- backend/src/institutions/institutions.service.ts
+++ backend/src/institutions/institutions.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InferInsertModel } from 'drizzle-orm';
+import { db } from 'src/db/database.module';
+import { institutions } from 'src/db/schema';
 
 @Injectable()
-export class InstitutionsService {}
+export class InstitutionsService {
+      async findAll() {
+            return db.select().from(institutions)
+        }
+    
+        async create(institutionsData: InferInsertModel <typeof institutions>) {
+            const result = await db.insert(institutions).values(institutionsData).returning();
+            return result[0];
+        }
+}
