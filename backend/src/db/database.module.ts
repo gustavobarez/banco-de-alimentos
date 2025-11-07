60fad1acec2f6834a739d038def32006e1b0f4a6
Gabs-bertolini <gabrielbertolinibarioni@gmail.com>
2025-11-06 21:29:42 -0300

    feat: dockerfile]

--- backend/src/db/database.module.ts
+++ backend/src/db/database.module.ts
@@ -9,7 +9,7 @@ import * as schema from './schema';
 
 const connectionString =
   process.env.DATABASE_URL ||
-  'postgresql://user:password@localhost:5432/food_bank';
+  'postgresql://postgres:postgres@localhost:5432/food_bank';
 
 const client = postgres(connectionString);
 export const db = drizzle(client, { schema });
